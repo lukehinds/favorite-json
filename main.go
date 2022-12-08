ba6741a82e7c359e90ff3c578db883121202dc8a
Luke Hinds <7058938+lukehinds@users.noreply.github.com>
2022-12-08 17:36:02 +0000

    Update main.go

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
     })
+    // comment
 
     http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
         fmt.Fprintf(w, "Hi")
